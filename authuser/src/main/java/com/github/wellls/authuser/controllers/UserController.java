5a8b3a20238e06a96fb4628922cc3117e3c21fbb
Wellington <wellington.map@hotmail.com>
2025-07-18 20:03:01 -0300

    feat(authuser): implementing hateoas on method getAllUsers

--- authuser/src/main/java/com/github/wellls/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/github/wellls/authuser/controllers/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/users")
 //@CrossOrigin(origins = "*", maxAge = 3600)
@@ -29,6 +32,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec, Pageable pageable){
         Page<UserModel> userModelPage = userService.findAll(spec, pageable);
+        if(!userModelPage.isEmpty()){
+            for(UserModel user : userModelPage.toList()){
+                user.add(linkTo(methodOn(UserController.class).getOneUser(user.getUserId())).withSelfRel());
+            }
+        }
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
--- authuser/src/main/java/com/github/wellls/authuser/models/UserModel.java
+++ authuser/src/main/java/com/github/wellls/authuser/models/UserModel.java
@@ -18,11 +18,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.springframework.hateoas.RepresentationModel;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
-public class UserModel implements Serializable {
+public class UserModel extends RepresentationModel<UserModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
